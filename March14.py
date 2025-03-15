7618c5b6fa045d936095b76abc384f3afe775fc0
ipekkkk404 <ipekd770@stemcollegiate.ca>
2025-03-15 10:57:11 -0600

    Update March14.py

--- March14.py
+++ March14.py
@@ -3,11 +3,11 @@ Today im doing day 6 because i forgot to save all my work last time i did it.
   print("Secure Login")
 username = input("What is your username?")
 password = input("What is your password?")
-if username == "David" and password == "PyTh0nR*cks":
-  print("Welcome, David! You are looking nice today!")
-elif username == "Becky" and password == "Repl!t4evEr":
-  print("Hi Becky! Love your hair today!")
-elif username == "Bill" and password == "SmashTHEb*gs!":
-  print("Yo! Bill! What up?!")
+if username == "Nichole" and password == "PyTh0nR*cks":
+  print("Welcome, Ipek! You are looking nice today!")
+elif username == "Nichole" and password == "Repl!t4evEr":
+  print("Hi Nichole! Love your hair today!")
+elif username == "Arabella" and password == "SmashTHEb*gs!":
+  print("Yo! Arabella! What up?!")
 else:
   print("You do not have access. Go away!")
